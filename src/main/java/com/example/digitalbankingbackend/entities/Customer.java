b5c9c4d326400fb04894f1b27bc089f89e94beb9
Abderrahmanedev23 <125931479+Abderrahmanedev23@users.noreply.github.com>
2025-05-17 19:45:32 +0100

    first commit

---
+++ src/main/java/com/example/digitalbankingbackend/entities/Customer.java
@@ -0,0 +1,22 @@
+package com.example.digitalbankingbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private List<BankAccount> bankAccounts;
+}
